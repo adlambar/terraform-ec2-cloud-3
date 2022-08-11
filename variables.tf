a840cc219bd0db838f608ffc2e4c849d131c4b69
adlambar <adlambar@yahoo.com.ar>
2022-08-11 10:52:50 -0300

    Add instance_ami variable

--- variables.tf
+++ variables.tf
@@ -13,3 +13,19 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
 }
 
+variable "instance_ami" {
+  description = "EC2 instance AMI"
+  default     = "ami-052efd3df9dad4825"
+}
+
+/*
+Free Tiers for N. Virginia
+
+Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type     ami-090fa75af13c156b4
+Red Hat Enterprise Linux 8 (HVM), SSD Volume Type           ami-06640050dc3f556bb
+SUSE Linux Enterprise Server 15 SP4 (HVM), SSD Volume Type  ami-08e167817c87ed7fd
+Ubuntu Server 22.04 LTS (HVM), SSD Volume Type              ami-052efd3df9dad4825
+Debian 11 (HVM), SSD Volume Type                            ami-09a41e26df464c548
+Microsoft Windows Server 2019 Base with Containers          ami-0c4fbf8692f7dd2da
+
+*/
