273bcc0e54e29cc1a751778559c5012bfabca71b
adlambar <adlambar@yahoo.com.ar>
2022-08-11 11:43:15 -0300

    Correction instance_name to outputs.pf

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@ output "instance_public_ip" {
   value = aws_instance.instance.public_ip
 }
 
-output "instance_public_ip" { 
+output "instance_name" { 
   value = "${lookup(aws_instance.instance.tags, "Name")}"
 }
